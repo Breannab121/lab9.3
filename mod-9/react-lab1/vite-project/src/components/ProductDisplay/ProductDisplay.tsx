a5d55f3fd68b9eb1d4fb45892baf6d57d22ed2e2
Breanna <breannab121@gmail.com>
2025-06-16 20:47:55 -0400

    add

---
+++ mod-9/react-lab1/vite-project/src/components/ProductDisplay/ProductDisplay.tsx
@@ -0,0 +1,17 @@
+// types/index.ts
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl?: string;
+  inStock: boolean;
+}
+ 
+export interface ProductDisplay {
+  product: Product;
+  showDescription?: boolean;
+  showStockStatus?: boolean;
+  onAddToCart?: (productId: string) => void;
+  children?: React.ReactNode;
+}
