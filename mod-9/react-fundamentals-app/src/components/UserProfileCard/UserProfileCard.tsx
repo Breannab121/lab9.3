a49a77ba24e01f224d4b0bc05f6f373733c8b46b
Breanna <breannab121@gmail.com>
2025-06-16 20:45:08 -0400

    fixed

--- mod-9/react-fundamentals-app/src/components/UserProfileCard/UserProfileCard.tsx
+++ mod-9/react-fundamentals-app/src/components/UserProfileCard/UserProfileCard.tsx
@@ -5,7 +5,6 @@ export const UserProfileCard: React.FC<UserProfileCardProps> = (props) => {
     return (
         <div>
             <p>Id: {props.user.name}</p>
-            <p>Email: {props.showEmail ? "john@gmail.com" : "null"} </p>
             <p>Role: {props.showRole ? "Admin" : "User"} </p>
         </div>
     )
